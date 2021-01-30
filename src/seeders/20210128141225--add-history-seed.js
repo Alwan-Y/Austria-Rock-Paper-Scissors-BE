544b7633e6111fb3bb11d7d2ef33b630cafd27a9
Hendra Wardana <maxnara19@gmail.com>
2021-01-30 23:30:42 +0700

    add column round

--- src/seeders/20210128141225--add-history-seed.js
+++ src/seeders/20210128141225--add-history-seed.js
@@ -3,13 +3,23 @@ module.exports = {
     await queryInterface.bulkInsert('History', [
       {
         id: '5041f349-ef85-4078-856d-d821fca998ed',
+        round: 1,
         playerOneChoice: 'rock',
         playerTwoChoice: 'paper',
         result: 'beta Win',
         roomId: 'c4cb353b-0f1e-455b-b634-562a6d39bc90',
       },
       {
+        id: '60de382c-1117-4c3f-bcb5-a02bfcb6888a',
+        round: 2,
+        playerOneChoice: 'rock',
+        playerTwoChoice: null,
+        result: null,
+        roomId: 'c4cb353b-0f1e-455b-b634-562a6d39bc90',
+      },
+      {
         id: '8c8ba6db-f07b-4b51-94b0-e7a3dd119a5a',
+        round: 1,
         playerOneChoice: 'rock',
         playerTwoChoice: 'paper',
         result: 'alpha Win',
@@ -17,6 +27,7 @@ module.exports = {
       },
       {
         id: 'f902d5ad-bee4-4975-8799-f7ecbd4ab98c',
+        round: 1,
         playerOneChoice: 'rock',
         playerTwoChoice: 'rock',
         result: 'DRAW',
