05818edff2016d00f9104386330430eda47bd94b
Hendra Wardana <maxnara19@gmail.com>
2021-02-06 13:35:23 +0700

    add realtime service using socketio

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import express from 'express'
+import http from 'http'
 import cors from 'cors'
 import apis from './routes/apis'
+import RealtimeService from './services/RealtimeService'
 
 require('dotenv').config()
 
 const app = express()
+
+const server = http.createServer(app)
+
 app.use(express.json())
 app.use(cors())
+app.set('RealtimeService', new RealtimeService(server, { cors: { origin: '*' } }))
 
 app.use('/apis', apis)
 
-app.use('/', apis)
 app.get('*', (req, res) => res.status(404).send('404 Not Found'))
 
-app.listen(process.env.APP_PORT, () => console.log(`running on ${process.env.APP_PORT}`))
+server.listen(process.env.APP_PORT, () => console.log(`running on ${process.env.APP_PORT}`))
