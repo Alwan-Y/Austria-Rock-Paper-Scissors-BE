6d3a48cf5410cfaf6738f85323f2fb17b3f51046
Hendra Wardana <maxnara19@gmail.com>
2021-02-07 15:23:31 +0700

    add join room validation

--- src/controllers/RoomController/index.js
+++ src/controllers/RoomController/index.js
@@ -47,31 +47,19 @@ class RoomController {
       const { roomId } = req.params
       const { username } = req.body
 
-      const room = await Room.findOne({
-        where: { id: roomId },
-      })
+      try {
+        const room = await Room.findOne({
+          where: { id: roomId },
+        })
 
-      if (!room) {
-        return res.status(400).json({ message: 'Not found' })
-      }
+        if (!room) {
+          return res.status(400).json({ message: 'Room does not exists' })
+        }
 
-      // eslint-disable-next-line max-len
-      const rejoinRoom = await Room.findOne({ where: { and: [{ id: roomId }, { playerOneUsername: username }, { playerTwoUsername: username }] } })
+        const { playerOneUsername, playerTwoUsername } = room.dataValues
 
-      if (rejoinRoom) {
-        return res.status(200).json({ roomId })
-      }
-
-      // eslint-disable-next-line max-len
-      const availableRoom = await Room.findOne({ where: { and: [{ id: roomId }, { playerTwoUsername: null }] } })
-
-      if (!availableRoom) {
-        return res.status(400).json({ message: 'Room is full' })
-      }
-
-      return room.update({ playerTwoUsername: username })
-        .then(
-          (updated) => Room.findOne(
+        if ([playerOneUsername, playerTwoUsername].includes(username)) {
+          return Room.findOne(
             {
               include: [{
                 model: History,
@@ -82,23 +70,55 @@ class RoomController {
               where: { id: roomId },
             },
           ).then(
-            (room) => {
-              if (!room) return res.status(400).json({ message: 'games does not exist' })
-
-              return res.status(200).json({ room })
-            },
+            (room) => res.status(200).json({ room }),
           ).catch(
             (e) => {
               console.log(e)
               return res.status(500).json({ message: 'Internal Server Error' })
             },
-          ),
-        ).catch(
-          (e) => {
-            console.log(e)
-            return res.status(500).json({ message: e })
-          },
-        )
+          )
+        }
+
+        const isRoomAvailable = !playerTwoUsername
+
+        if (!isRoomAvailable) {
+          return res.status(400).json({ message: 'Room is full' })
+        }
+
+        return room.update({ playerTwoUsername: username })
+          .then(
+            (updated) => Room.findOne(
+              {
+                include: [{
+                  model: History,
+                  attributes: ['round', 'playerOneChoice', 'playerTwoChoice', 'result'],
+                  order: [['round', 'DESC']],
+                  limit: 1,
+                }],
+                where: { id: roomId },
+              },
+            ).then(
+              (room) => {
+                if (!room) return res.status(400).json({ message: 'games does not exist' })
+
+                return res.status(200).json({ room })
+              },
+            ).catch(
+              (e) => {
+                console.log(e)
+                return res.status(500).json({ message: 'Internal Server Error' })
+              },
+            ),
+          ).catch(
+            (e) => {
+              console.log(e)
+              return res.status(500).json({ message: e })
+            },
+          )
+      } catch (error) {
+        console.log(e)
+        return res.status(500).json({ message: 'Internal Server Error' })
+      }
     }
 }
 
